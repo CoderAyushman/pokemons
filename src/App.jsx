0fcb0bffa98bc9e7bcad03f4ff2833712bce5b5d
CoderAyushman <mishraayushman361@gmail.com>
2024-09-22 20:29:12 +0530

    adding header text

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import FatchData from "./components/FetchData";
 function App() {
   return (
     <div className="App">
+      <h1 className="header">POKEMONS</h1>
       <FatchData />
     </div>
   );
